c503d4b4a6e41e2a8c37af1626b29b764c58ba54
clarine <antibaclarine.com>
2023-02-16 07:10:42 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,37 @@
+fun main() {
+characters()
+ println( stmt("clarine" ,21))
+length()
+    print("Clarine")
+}
+fun characters(){
+    val institute = "AkiraChix"
+    print(institute[0])
+    print(institute[2])
+    print(institute[3])
+    println()
+}
+fun stmt(name:String,age:Int):String{
+   val name= "Clarine"
+    val age= 21
+    val stmt ="Hi my name is $name and i am $age years old"
+return stmt
+}
+fun length(){
+    var string="Software"
+    println(string.length)
+}
+fun print(name:String) {
+    var M = "Clarine"
+    if (name.equals(M)){
+
+      println("That's me")
+    }
+    else {
+    println("I don't what that is")
+}
+}
+
+
+
+
